fca748874d2e5945077a841a4cfd0283334ae414
Arthur Vieira Moreira Lucena <arthurvmx5@gmail,com>
2012-11-15 23:43:23 -0200

    Web site php

---
+++ sandbox/sitebeta/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="keywords" content="site, facebook, twitter, barato, preço, baixo, webdeveloper, desenvolvedor, sites, consitucionais, arte final, comics, gráficos, design grafico">
+    <meta http-equiv="description" content="Webdeveloper é Designer, sites constitucionais/dinâmicos, Logos, Artes, rapido desenvolvimento/cria, ótima tecnologia e ótimos preços, ">
+	<link rel="index" href="/home/" title="Home">
+	<link rel="help" href="/bio/" title="Bio">
+    <title>Titulo</title>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="navegation">
+			<ul>
+				<li><a class="link" href="http://localhost/sitebeta/home">Home</a></li>
+				<li><a class="link" href="http://localhost/sitebeta/bio">Bio</a></li>		
+			</ul>
+		</div>
+		<div id="loader">
+			<?php echo $_GET['page'] ?>
+		</div>
+	</div>
+</body>
+</html>
