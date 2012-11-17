ac162be0f02970569b4fe28b74f3eb07181e9135
Arthur Vieira Moreira Lucena <arthurvmx5@gmail,com>
2012-11-17 14:01:54 -0200

    Commit do beta do web site

--- sandbox/sitebeta/index.php
+++ sandbox/sitebeta/index.php
@@ -1,24 +1,39 @@
+<?php include '/lib/Includes.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="keywords" content="site, facebook, twitter, barato, preço, baixo, webdeveloper, desenvolvedor, sites, consitucionais, arte final, comics, gráficos, design grafico">
-    <meta http-equiv="description" content="Webdeveloper é Designer, sites constitucionais/dinâmicos, Logos, Artes, rapido desenvolvimento/cria, ótima tecnologia e ótimos preços, ">
-	<link rel="index" href="/home/" title="Home">
-	<link rel="help" href="/bio/" title="Bio">
-    <title>Titulo</title>
+	<?php include '/templates/header.php'; ?>
 </head>
 <body>
 	<div id="wrapper">
 		<div id="navegation">
 			<ul>
-				<li><a class="link" href="http://localhost/sitebeta/home">Home</a></li>
-				<li><a class="link" href="http://localhost/sitebeta/bio">Bio</a></li>		
+				<li><a class="link" href="<?php echo getBaseURL(); ?>home/">Home</a></li>
+				<li><a class="link" href="<?php echo getBaseURL(); ?>bio/">Bio</a></li>		
 			</ul>
 		</div>
 		<div id="loader">
-			<?php echo $_GET['page'] ?>
+			<?php 
+				if (isset($_GET['page'])) {
+					include '/pages/'.$_GET['page'].'.php';
+				} else {
+					include '/pages/home.php';
+				}
+			?>
 		</div>
 	</div>
+	<script src="<?php echo getBaseURL(); ?>js/modernizr.js"></script>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="<?php echo getBaseURL(); ?>js/script.js"></script>
 </body>
 </html>
+<!-- 
+inspiração:
+http://www.amazeelabs.com/en
+http://luciasoto.es/
+http://lab.4muladesign.com/dribbble/
+http://www.4muladesign.com/
+
+ler:
+http://tableless.com.br/mobile-first-a-arte-de-pensar-com-foco/
+ -->
